d6da1eccf9e8bd2e5300d9c650b311a1ce7b767c
ElliX <codeellix@gmail.com>
2025-04-08 20:42:19 +0200

    feat: keyboard symbols display is configured

--- simulator/src/app/keyboard/page.jsx
+++ simulator/src/app/keyboard/page.jsx
@@ -7,7 +7,7 @@ import { setCurrentColorPressedKey } from '../redux/panelToolsSlice';
 const Keyboard = (props) => {
     const dispatch = useDispatch();
     const activeKeyRef = useRef(null);
-    
+
     const textRight = useSelector(state => state.panelTools.textRight);
     let activeKey;
     if (textRight && textRight !== "Пробіл") {
@@ -29,7 +29,14 @@ const Keyboard = (props) => {
     const shiftLeft = new Set(["Y", "U", "I", "O", "P", "H", "J", "K", "L", "B", "N", "M", "^", "&", "*", "(", ")", "_", "+", "\"", ":", "<", ">", "?", "|", "{", "}", "Н", "Г", "I", "Щ", "З", "Р", "О", "Л", "Д", "И", "Т", "Ь", "Х", "Ж", "Э", "Б", "Ю", "Ї", "Є"]);
     const shiftRight = new Set(["T", "R", "E", "W", "Q", "G", "F", "D", "S", "A", "V", "C", "X", "Z", "Е", "К", "У", "Ц", "Й", "П", "А", "В", "Ё", "Ф", "М", "С", "Ч", "Я", "І", "%", "$", "#", "@", "!", "~"]);
 
-    const highlightShiftLeft = isUpperCase && shiftLeft.has(activeKey);
+    const baseLang = props.lang?.split('_')[0];
+    const currentShiftLeft = new Set(shiftLeft);
+    if (baseLang === 'ru' || baseLang === 'uk') {
+        currentShiftLeft.add(',');
+        currentShiftLeft.add('/');
+    }
+
+    const highlightShiftLeft = isUpperCase && currentShiftLeft.has(activeKey);
     const highlightShiftRight = isUpperCase && shiftRight.has(activeKey);
 
     const baseLayouts = {
@@ -56,7 +63,94 @@ const Keyboard = (props) => {
         ]
     };
 
-    const baseLang = props.lang?.split('_')[0];
+    if (textRight.charAt(0) === '!') {
+        baseLayouts.en[0][1] = '!';
+        baseLayouts.ru[0][1] = '!';
+        baseLayouts.uk[0][1] = '!';
+    } else if (textRight.charAt(0) === '+') {
+        baseLayouts.en[0][12] = '+';
+        baseLayouts.ru[0][12] = '+';
+        baseLayouts.uk[0][12] = '+';
+    } else if (textRight.charAt(0) === '<') {
+        baseLayouts.en[3][8] = '<';
+        baseLayouts.ru[3][8] = '<';
+        baseLayouts.uk[3][8] = '<';
+    } else if (textRight.charAt(0) === '>') {
+        baseLayouts.en[3][9] = '>';
+        baseLayouts.ru[3][9] = '>';
+        baseLayouts.uk[3][9] = '>';
+    } else if (textRight.charAt(0) === '?') {
+        baseLayouts.en[3][10] = '?';
+        baseLayouts.ru[0][7] = '?';
+        baseLayouts.uk[0][7] = '?';
+    } else if (textRight.charAt(0) === '{') {
+        baseLayouts.en[1][11] = '{';
+        baseLayouts.ru[1][11] = '{';
+        baseLayouts.uk[1][11] = '{';
+    } else if (textRight.charAt(0) === '}') {
+        baseLayouts.en[1][12] = '}';
+        baseLayouts.ru[1][12] = '}';
+        baseLayouts.uk[1][12] = '}';
+    } else if (textRight.charAt(0) === '(') {
+        baseLayouts.en[0][9] = '(';
+        baseLayouts.ru[0][9] = '(';
+        baseLayouts.uk[0][9] = '(';
+    } else if (textRight.charAt(0) === ')') {
+        baseLayouts.en[0][10] = ')';
+        baseLayouts.ru[0][10] = ')';
+        baseLayouts.uk[0][10] = ')';
+    } else if (textRight.charAt(0) === ':') {
+        baseLayouts.en[2][10] = ':';
+        baseLayouts.ru[0][6] = ':';
+        baseLayouts.uk[0][6] = ':';
+    } else if (textRight.charAt(0) === '|') {
+        baseLayouts.en[1][13] = '|';
+        baseLayouts.ru[1][13] = '|';
+        baseLayouts.uk[1][13] = '|';
+    } else if (textRight.charAt(0) === '"') {
+        baseLayouts.en[2][11] = '"';
+        baseLayouts.ru[0][2] = '"';
+        baseLayouts.uk[0][2] = '"';
+    } else if (textRight.charAt(0) === '@') {
+        baseLayouts.en[0][2] = '@';
+        baseLayouts.ru[0][2] = '@';
+        baseLayouts.uk[0][2] = '@';
+    } else if (textRight.charAt(0) === '_') {
+        baseLayouts.en[0][11] = '_';
+        baseLayouts.ru[0][11] = '_';
+        baseLayouts.uk[0][11] = '_';
+    } else if (textRight.charAt(0) === '#') {
+        baseLayouts.en[0][3] = '#';
+        baseLayouts.ru[0][3] = '#';
+        baseLayouts.uk[0][3] = '#';
+    } else if (textRight.charAt(0) === '$') {
+        baseLayouts.en[0][4] = '$';
+        baseLayouts.ru[0][4] = '$';
+        baseLayouts.uk[0][4] = '$';
+    } else if (textRight.charAt(0) === '%') {
+        baseLayouts.en[0][5] = '%';
+        baseLayouts.ru[0][5] = '%';
+        baseLayouts.uk[0][5] = '%';
+    } else if (textRight.charAt(0) === '*') {
+        baseLayouts.en[0][8] = '*';
+        baseLayouts.ru[0][8] = '*';
+        baseLayouts.uk[0][8] = '*';
+    } else if (textRight.charAt(0) === '^') {
+        baseLayouts.en[0][6] = '^';
+        baseLayouts.ru[0][6] = '^';
+        baseLayouts.uk[0][6] = '^';
+    } else if (textRight.charAt(0) === '&') {
+        baseLayouts.en[0][7] = '&';
+        baseLayouts.ru[0][7] = '&';
+        baseLayouts.uk[0][7] = '&';
+    } else if (textRight.charAt(0) === ',') {
+        baseLayouts.ru[3][10] = ',';
+        baseLayouts.uk[3][10] = ',';
+    } else if (textRight.charAt(0) === '/') {
+        baseLayouts.ru[1][13] = '/';
+        baseLayouts.uk[1][13] = '/';
+    }
+
     const layout = baseLayouts[baseLang] || baseLayouts.en;
 
     return (
--- simulator/src/app/panel-tools/page.jsx
+++ simulator/src/app/panel-tools/page.jsx
@@ -36,7 +36,7 @@ const PanelToolsContent = () => {
     const maxTextLength = 16;
     const searchParams = useSearchParams();
     const setTimer = lang === "en" || lang === "ru" || lang === "uk";
-    const ignoredKeys = [' ', 'Space', 'Shift', 'Control', 'Alt', 'Meta', 'Tab', 'Escape', 'Enter', 'Backspace', 'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'Insert', 'Home', 'End', 'PageUp', 'PageDown', 'NumLock'];
+    const ignoredKeys = [' ', 'Space', 'Shift', 'Control', 'Alt', 'Meta', 'Tab', 'Escape', 'Enter', 'Backspace', 'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'Insert', 'Home', 'End', 'PageUp', 'PageDown', 'NumLock', ',', '.', '?', '<', '>', '/', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '=', '+', '|', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '}', '{', '[', ']'];
 
     const fingerPositions = {
         leftHand: {
